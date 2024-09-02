c4f4a617a94338a89b672eb2d60d01b498f73151
Dhuman Hardwar <danny.hardawar@gmail.com>
2024-09-02 14:41:57 -0400

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,35 @@
+#Danny Hardawar
+#Notes from Chapter 3 - Python Crash Course - By Eric Matthews
+
+#Square Brackets indicate a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#To access elements in a list, you tell python of the index
+print(bicycles[0])
+
+#using string methods 
+print(bicycles[0].title())
+
+#index positions start at 0
+
+#Using individual values
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
+
+
+#Using append to build a list automatically
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+#Inserting a new element
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+#The pop method removes the last item in the list.
+#But allows you to work with the item after removing it.
+#The term pop relates to list like a stack of items.
+#You pop one item off the top of the stack
---
+++ loops.py
@@ -0,0 +1,37 @@
+#Danny Hardawar
+#Notes from Chapter 4 - Python Crash Course - By Eric Matthews
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+#for magician in magicians
+#Tells python to retreieve the first value from the list magicians and associate it with the variable magician.
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick {magician.title()}.\n")
+
+#Using Range
+for value in range(1,5):
+    print(value)
+
+for value in range(1,6):
+    print(value)
+
+#using range to make a list
+numbers = list(range(1, 6))
+print(numbers)
+
+#defiining a tuple
+#A tuple is like a list but you use parentheses instead of brackets.
+# Once you define a tuple you can access individual elements using an index
+
+dimensions = (200 , 50)
+print(dimensions[0])
+print(dimensions[1])
+
+#looping through a tuple like looping through a list
+for dimension in dimensions:
+    print(dimension)
