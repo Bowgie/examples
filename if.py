8908d5a6ba80e545e12174e5fe3f213e0482ef7b
Dhuman Hardwar <danny.hardawar@gmail.com>
2024-09-02 17:07:04 -0400

    upload if.py

---
+++ if.py
@@ -0,0 +1,13 @@
+#Danny Hardawar
+#Python Crash Course - Chap 5 - Eric Matthes
+
+#If the value of the car is bmw, print it in uppercase.
+cars = ['audi', 'bmw', 'subaru', 'toyoya']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+        
